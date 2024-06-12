5a2ac3058d5a3e638c50cda9a8c203d745dd7b35
Alejandro Reyes <reyesbalejandroe@gmail.com>
2024-06-12 16:09:10 +0200

    add/sql

---
+++ demo_express_routing_sql/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express(); // Inicializar servidor
+const port = 3000;
+
+// Rutas
+const booksRoutes = require("./routes/books.routes")
+const productsRoutes = require("./routes/products.routes")
+const entriesRoutes = require("./routes/entries.routes")
+
+app.use(express.json()); // Habilito recepción de JSON en servidor
+
+app.get("/", (req, res) => {
+  res.send("Hello Wodffdrld let's go for cofedfd!");
+});
+
+// Rutas
+//API
+app.use('/api/books',booksRoutes);
+app.use('/api/products',productsRoutes);
+app.use('/api/entries',entriesRoutes);
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`);
+});
