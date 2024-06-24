860c7e22ac05ac3f5de8141c1bedc398256b4fb8
Alejandro Reyes <reyesbalejandroe@gmail.com>
2024-06-24 10:27:40 +0200

    add/validator

---
+++ demo_express_routing_sql_middlewares_mongo_validator/validators/books.validators.js
@@ -0,0 +1,45 @@
+const {param, query, body} = require("express-validator");
+
+
+const validateCreateBook =  [
+   body("title")
+     .exists()
+     .withMessage("Title of book is required")
+     .isString()
+     .withMessage("Title should be string"),
+   body("author")
+     .exists()
+     .withMessage("Book author is required")
+     .isString()
+     .withMessage("Author should be string")
+     .isLength({ min: 5 })
+     .withMessage("Author should be at least 5 characters"),
+   body("pages")
+     .exists()
+     .withMessage("Book pages is required")
+     .isNumeric()
+     .withMessage("Pages must be numeric"),
+   body("year")
+     .exists()
+     .withMessage("Year is required")
+     .isNumeric()
+     .withMessage("Year must be numeric"),
+   body("description")
+     .exists()
+     .withMessage("Description is required")
+     .isString()
+     .withMessage("Description should be string")
+     .isLength({ min: 500 })
+     .withMessage("Description at least 500 characters") 
+   ];
+
+ const validateDeleteBook =  [
+   param('title')
+   .isLength({ max: 70 })
+   .withMessage("Title should have max 70 characters to delete a book")
+]
+
+module.exports = {
+   validateCreateBook,
+   validateDeleteBook
+}
